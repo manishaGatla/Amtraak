0cb72e2837184cd44c6db333988512547e1b1ef6
pxk23480 <pxk23480@ucmo.edu>
2023-12-05 02:13:06 -0600

    Fixed Bug

--- LoginController.py
+++ LoginController.py
@@ -4,24 +4,27 @@ try:
 except:
     print("unable to connect or already in running state")
 
-def authenticate_user(username, password, isAdmin):
+def authenticate_user(email, password):
     
     if db is not None:
         admin_collection = db["Admins"]
-        passenger_collection = db["Passengers"]
+        passenger_collection = db["Customers"]
 
-        if(isAdmin):
-            if(admin_collection.find_one({"username": username, "password": password})):
-                return "admin"
-        else:
-            if(passenger_collection.find_one({"username": username, "password": password})):
-                return "passenger"
+        
+        valid = admin_collection.find_one({"username": email, "password": password})
+        print(valid)
+        if(valid):
+            return 
+        
+    
+        if(passenger_collection.find_one({"username": email, "password": password})):
+            return "passenger"
 
     return None
 def Register(data):
     customers_collection  = db["Customers"]
 
-    data_insert = customers_collection.insert_one(data.get('name'),data.get('email'),data.get('phoneNumber'),data.get('password'),data.get('Password'),data.get('dob'),data.get('gender'))
+    data_insert = customers_collection.insert_one(data)
     if(data_insert):
         return "success"
     return None
--- Main.py
+++ Main.py
@@ -1,35 +1,33 @@
+from fastapi import FastAPI
+from fastapi.templating import Jinja2Templates
 from flask import Flask, render_template, request, jsonify
+import uvicorn
 import LoginController
 from MongoConnectionHelper import connect_to_mongodb
 
+# app = FastAPI()
 app = Flask(__name__)
-
-@app.route('/login', methods=['GET'])
+templates = Jinja2Templates(directory="templates")
+@app.get('/login')
 def login():
     data = request.get_json()
-    username = data.get('username')
+    username = data.get('email')
     password = data.get('password')
-    isAdmin  = data.get('isAdmin')
+    
 
-    user_type = LoginController.authenticate_user(username, password, isAdmin)
+    user_type = LoginController.authenticate_user(username, password)
     
     if user_type:
         return jsonify({"success": True, "user_type": user_type})
     else:
         return jsonify({"success": False, "message": "Invalid username or password"})
-@app.route('/register', methods=['POST'])
+@app.post('/register')
 def Register():
+
+    print("...................................................................")
+    print("request")
     data = request.get_json()
     print(data)
-    # name = data.get('name')
-    # email = data.get('email')
-    # password = data.get('password')
-    # phoneNumber = data.get('phoneNumber')
-    # dob = data.get('dob')
-    # gender = data.get('gender')
-
-    
-
     register = LoginController.Register(data)
     if register:
         return jsonify({"Success": True})
@@ -37,6 +35,9 @@ def Register():
         return jsonify({"Success":False,"message":"Unable to register! Please contact Admin"})
 @app.route('/')
 def root():
-  return render_template('index.html')
+  return templates.TemplateResponse("index.html", {"request": request})
+
+
 if __name__ == "__main__":
+    #uvicorn.run("Main:app", host="127.0.0.1", port="5000",reload=False,log_level="info")
     app.run(port=5000,debug=True)
