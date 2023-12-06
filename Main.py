09a518f3929839df823df35bb03fd34546b0dd6f
pxk23480 <pxk23480@ucmo.edu>
2023-12-06 15:20:58 -0600

    fixed cors

--- Main.py
+++ Main.py
@@ -9,7 +9,21 @@ from flask_cors import CORS
 
 # app = FastAPI()
 app = Flask(__name__)
-CORS(app)
+CORS(app,resources={r"/*": {"origins": "*"}})
+# origins = [
+#     "http://localhost.tiangolo.com",
+#     "https://localhost.tiangolo.com",
+#     "http://localhost",
+#     "http://localhost:4200",
+#     "http://localhost:8080",
+# ]
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 templates = Jinja2Templates(directory="templates")
 @app.get('/getAllDetails/<string:email>')
 def login(email):
